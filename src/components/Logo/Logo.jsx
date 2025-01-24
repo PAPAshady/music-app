ac66009c9c5a52a1615ab3fea4b262332de0face
Nima Zamani <zamani.nima18@gmail.com>
2025-01-24 13:54:55 +0330

    feat(App.css, assets, Logo.jsx): added 'Logo.jsx' component and added font family to body tag of the html

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,48 @@
+import LogoImg from '../../assets/images/Logo/Logo.png';
+import PropTypes from 'prop-types';
+
+export default function Logo({ size }) {
+  const logoSize = {
+    sm: 'w-[30px] h-[23px]',
+    md: 'w-14 h-11',
+    lg: 'w-[100px] h-[80px]',
+    xl: 'w-[412px h-[317px] mt-16',
+  };
+
+  const textSize = {
+    md: 'text-sm mt-1 font-medium',
+    lg: 'subheading-1 font-bold mt-4',
+    xl: 'text-7xl mt-16 font-bold',
+  };
+
+  // define diffrent color for each letter of logo text
+  const letterColors = [
+    'text-primary-100',
+    'text-primary-200',
+    'text-primary-300',
+    'text-primary-400',
+    'text-primary-300',
+    'text-primary-200',
+    'text-primary-100',
+  ];
+
+  return (
+    <div className="inline-flex flex-col items-center">
+      <img className={`${logoSize[size]}`} src={LogoImg} alt="VioTune" />
+      {size !== 'sm' && (
+        <div className={`text-center ${textSize[size]}`}>
+          {/* set diffrent color for each letter of logo text */}
+          {'VioTune'.split('').map((char, index) => (
+            <span key={index} className={letterColors[index]}>
+              {char}
+            </span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+Logo.propTypes = {
+  size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl']).isRequired,
+};
