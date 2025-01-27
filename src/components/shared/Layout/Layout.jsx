96da8dc0922582d3664555ca436d96bc98636de1
Nima Zamani <zamani.nima18@gmail.com>
2025-01-27 22:33:21 +0330

    feat(App.css, assets, DesktopNavbar.jsx, Layout.jsx): Enhanced UI and Layout Consistency
    - Updated container width to 95% for improved responsiveness.
    - Added background images for each page, enhancing visual appeal.
    - Implemented dynamic handling of background images for each page, ensuring seamless integration.
    - Unified layout structure across all pages, promoting consistency and a cohesive user experience.

--- src/components/shared/Layout/Layout.jsx
+++ src/components/shared/Layout/Layout.jsx
@@ -1,10 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import homePageBgImg from '../../../assets/images/backgrounds/home-page.jpg';
+import favoritesPageBgImg from '../../../assets/images/backgrounds/favorites-page.jpg';
+import playlistAndSubscriptionPageBgImg from '../../../assets/images/backgrounds/playlist and-subscription-page.jpg';
+import browsePageBgImg from '../../../assets/images/backgrounds/browse-page.jpg';
+import Header from '../Header/Header';
+import DesktopNavbar from '../../DesktopNavbar/DesktopNavbar';
+import { useLocation, Outlet } from 'react-router-dom';
 
 export default function Layout() {
+  const currentPage = useLocation().pathname;
+
+  const pagesBackgrounds = {
+    '/': homePageBgImg,
+    '/favorites': favoritesPageBgImg,
+    '/playlist': playlistAndSubscriptionPageBgImg,
+    '/browse': browsePageBgImg,
+    '/subscription': playlistAndSubscriptionPageBgImg,
+  };
+
   return (
-    <div>
-      Layout
-      <Outlet />
+    <div className="bg-primary-800 relative min-h-[100dvh]">
+      <div
+        className="absolute size-full bg-cover bg-center bg-no-repeat opacity-10 blur-lg"
+        style={{ backgroundImage: `url(${pagesBackgrounds[currentPage]})` }}
+      ></div>
+      <div className="z-10 w-full pt-4">
+        <div className="container">
+          <Header />
+        </div>
+      </div>
+
+      <main className="text-secondary-50 relative container flex h-[5000px] gap-8">
+        <div className="relative hidden lg:block">
+          <div className="sticky top-0 z-10 pt-6">
+            <DesktopNavbar />
+          </div>
+        </div>
+        <div className="pt-6">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 }
