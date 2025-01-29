8c9f9535979a78cdd59cf8f35c1a2300f7c89c4d
Nima Zamani <zamani.nima18@gmail.com>
2025-01-29 22:14:33 +0330

    feat(App.css, assets, IconButton.jsx, PlayBar.jsx, SidebarPlaylist.jsx, Layout.jsx, Home.jsx): created SidebarPlaylist component and it's dependencies. also added global scrollbar styles.

---
+++ src/components/MusicCards/PlayBar/PlayBar.jsx
@@ -0,0 +1,121 @@
+import { memo } from 'react';
+import { Heart, Menu, Play } from 'iconsax-react';
+import IconButton from '../../Buttons/IconButton/IconButton';
+import noCoverImg from '../../../assets/images/covers/no-cover.jpg';
+import PropTypes from 'prop-types';
+
+const PlayBar = memo(
+  ({
+    size,
+    title,
+    cover = noCoverImg,
+    artist = 'Unknown artist',
+    time,
+    album = 'Unknown album',
+    isLiked,
+    clickHandler,
+    classNames,
+  }) => {
+    const wrapperSizes = {
+      lg: 'max-w-[410px] lg:max-w-[890px]',
+      md: 'max-w-[320px] lg:max-w-[510px]',
+      sm: 'max-w-[320px]',
+    };
+
+    const musicTitleSizes = {
+      lg: 'text-base lg:text-xl',
+      md: 'paragraph-1',
+      sm: 'text-xs',
+    };
+
+    const albumNameSizes = {
+      lg: 'text-base',
+      md: 'text-sm',
+      sm: 'hidden',
+    };
+
+    return (
+      <div
+        className={`bg-primary-800/60 hover:inset-shadow-secondary-400 border-primaty-10 group hover:bg-primary-700/40 lg:hover:bg-primary-800 flex items-center justify-between gap-4 rounded-lg border p-1 inset-shadow-transparent transition-all duration-300 lg:inset-shadow-[2px_2px_15px] ${wrapperSizes[size]} ${classNames}`}
+      >
+        <div className="flex grow gap-2">
+          <button className="relative overflow-hidden rounded-md" onClick={clickHandler}>
+            <img
+              src={cover}
+              className={`size-12 object-cover ${size !== 'sm' ? 'lg:size-14' : ''}`}
+              alt={title}
+            />
+            <span
+              className={`absolute top-1/2 left-1/2 flex size-full -translate-x-1/2 -translate-y-1/2 items-center justify-center bg-transparent opacity-0 transition-all duration-300 group-hover:bg-black/60 group-hover:opacity-100`}
+            >
+              <span className="size-6 lg:size-8">
+                <Play size="100%" />
+              </span>
+            </span>
+          </button>
+          <div className="flex flex-col">
+            <button
+              className={`text-white-50 grow text-start ${musicTitleSizes[size]}`}
+              onClick={clickHandler}
+            >
+              {title}
+            </button>
+            <span
+              className={`text-secondary-200 grow ${size === 'lg' ? 'text-xs lg:text-sm' : 'caption-2'}`}
+            >
+              {artist}
+            </span>
+          </div>
+        </div>
+
+        <div
+          className={`flex items-center justify-between gap-4 pe-3 ${size !== 'sm' ? 'lg:grow-3' : ''}`}
+        >
+          {size !== 'sm' && (
+            <>
+              <p className={`hidden lg:block ${albumNameSizes[size]}`}>{album}</p>
+              <span
+                className={`text-secondary-200 pe-3 text-xs lg:p-0 ${size === 'lg' ? 'lg:text-sm' : 'hidden lg:block'}`}
+              >
+                {time}
+              </span>
+            </>
+          )}
+          <div className={`flex items-center gap-2 ${size !== 'sm' ? 'lg:gap-4' : ''}`}>
+            <div className={` ${size === 'md' ? 'block lg:hidden' : 'block'}`}>
+              <IconButton
+                icon={
+                  <Heart
+                    size={size === 'sm' ? 16 : 24}
+                    className={`transition-colors ${isLiked ? 'fill-red-500 text-red-500' : ''}`}
+                  />
+                }
+              />
+            </div>
+            <div
+              className={`${size === 'md' ? 'hidden lg:block' : ''} ${size === 'sm' ? 'hidden lg:block' : ''}`}
+            >
+              <IconButton icon={<Menu size={size === 'sm' ? 16 : 24} />} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+PlayBar.displayName = 'PlayBar';
+
+PlayBar.propTypes = {
+  size: PropTypes.oneOf(['sm', 'md', 'lg']).isRequired,
+  title: PropTypes.string.isRequired,
+  cover: PropTypes.string,
+  artist: PropTypes.string,
+  time: PropTypes.string.isRequired,
+  album: PropTypes.string,
+  clickHandler: PropTypes.func,
+  isLiked: PropTypes.bool,
+  classNames: PropTypes.string,
+};
+
+export default PlayBar;
---
+++ src/components/SidebarPlaylist/SidebarPlaylist.jsx
@@ -0,0 +1,76 @@
+import { cloneElement } from 'react';
+import { Menu, Music, Timer, User } from 'iconsax-react';
+import PropTypes from 'prop-types';
+import PlaylistImg from '../../assets/images/backgrounds/login-signup page.jpg';
+import PlayBar from '../MusicCards/PlayBar/PlayBar';
+
+export default function SidebarPlaylist({ playList }) {
+  const PlaylistInfosArray = [
+    { id: 1, title: `${playList.length} Tracks`, icon: <Music /> },
+    { id: 2, title: '01:11:58', icon: <Timer /> },
+    { id: 3, title: 'Rayan', icon: <User /> },
+  ];
+
+  return (
+    <div className="bg-secondary-400/40 border-secondary-200 flex h-[calc(100dvh-150px)] max-h-[760px] min-h-[430px] w-[300px] flex-col rounded-xl border px-3 pt-5 pb-4 xl:w-[350px]">
+      <div className="flex items-center justify-between gap-1">
+        <p className="text-white-50 subheading-3 truncate">Sad playlist</p>
+        <button className="text-secondary-50">
+          <Menu />
+        </button>
+      </div>
+
+      <div className="my-6 flex gap-2">
+        <div>
+          <img
+            src={PlaylistImg}
+            alt=""
+            className="size-32 rounded-[10px] object-cover xl:size-[160px]"
+          />
+        </div>
+        <div className="flex flex-col">
+          {PlaylistInfosArray.map((info) => (
+            <PlaylistInfo key={info.id} {...info} />
+          ))}
+        </div>
+      </div>
+
+      <div id="playlist-songs-wrapper" className="flex grow flex-col gap-2 overflow-y-auto pe-2">
+        {playList.map((song) => (
+          <PlayBar key={song.id} size="sm" {...song} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function PlaylistInfo({ title, icon }) {
+  const styledIcon = cloneElement(icon, { size: 18 });
+  return (
+    <div className="flex grow items-center gap-1">
+      {styledIcon}
+      <span className="max-w-[90px] truncate text-sm xl:text-base">{title}</span>
+    </div>
+  );
+}
+
+SidebarPlaylist.propTypes = {
+  playList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      time: PropTypes.string.isRequired,
+      album: PropTypes.string,
+      artist: PropTypes.string,
+      cover: PropTypes.string,
+      isLiked: PropTypes.bool,
+      classNames: PropTypes.string,
+      clickHandler: PropTypes.func,
+    })
+  ),
+};
+
+PlaylistInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+};
