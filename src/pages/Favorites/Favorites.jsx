ddc2478e9a7ee9b9e672f6b94c612a973eccd13d
Nima Zamani <zamani.nima18@gmail.com>
2025-02-17 17:54:49 +0330

    feat(favorites): added suggestions section to favorites page.

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,19 @@
 import bannerBg from '../../assets/images/backgrounds/player-and-settings-page.png';
 import SidebarPlaylist from '../../components/SidebarPlaylist/SidebarPlaylist';
 import PlayBar from '../../components/MusicCards/PlayBar/PlayBar';
+import SectionTitle from '../../components/SectionHeader/SectionHeader';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import { songs } from '../../data';
+import { shuffleArray, chunkArray } from '../../utils/arrayUtils';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export default function Favorites() {
   const isTablet = useMediaQuery('(min-width: 480px)');
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+
   return (
     <div className="flex w-full items-start gap-6">
       <div className="flex grow flex-col gap-8 lg:gap-10">
@@ -22,7 +30,6 @@ export default function Favorites() {
             </p>
           </div>
         </div>
-
         <div className="sm: flex max-h-[280px] flex-col gap-3 overflow-y-auto px-3 sm:max-h-[360px] lg:max-h-[414px] lg:gap-4">
           {songs.map((song) => (
             <PlayBar
@@ -32,6 +39,50 @@ export default function Favorites() {
               classNames="!max-w-full"
             />
           ))}
+        </div>
+        <div>
+          <SectionTitle title="Seggestions for you" />
+          <div className="mx-auto w-[95%] xl:max-w-[940px]">
+            <Swiper
+              slidesPerView={1}
+              modules={[Pagination]}
+              spaceBetween={12}
+              pagination={{ clickable: true }}
+              breakpoints={{
+                360: {
+                  slidesPerView: 1.2,
+                },
+                480: {
+                  slidesPerView: 1.5,
+                },
+                640: {
+                  slidesPerView: 2,
+                },
+                768: {
+                  slidesPerView: 2.4,
+                },
+                1024: {
+                  slidesPerView: 3,
+                },
+              }}
+              className="max-w-[95dvw] lg:max-w-[calc(95dvw-86px)] xl:max-w-[calc(95dvw-410px)]"
+            >
+              {chunkArray(shuffleArray(songs), 2).map((songsArray, index) => (
+                <SwiperSlide key={index} className="pb-11">
+                  <div className="flex flex-col gap-3">
+                    {songsArray.map((song) => (
+                      <PlayBar
+                        key={song.id}
+                        {...song}
+                        size={isDesktop ? 'sm' : 'md'}
+                        classNames="!max-w-full p-[1px]"
+                      />
+                    ))}
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
       </div>
       <SidebarPlaylist playList={songs} />
