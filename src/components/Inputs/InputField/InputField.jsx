5276f3c750cf5b7f76329ebf0f38a897d69b22ac
Nima Zamani <zamani.nima18@gmail.com>
2025-02-25 15:31:47 -0800

    feat(InputField.jsx): added 'InputField.jsx' component to the project.

---
+++ src/components/Inputs/InputField/InputField.jsx
@@ -0,0 +1,28 @@
+import { useId } from 'react';
+import PropTypes from 'prop-types';
+
+export default function InputField({ value, onChange, placeholder = 'Text', classNames }) {
+  const id = useId();
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="text-primary-100 text-sm lg:text-base" htmlFor={id}>
+        {placeholder}
+      </label>
+      <input
+        className={`text-primary-50 border-primary-50 bg-primary-900 focus:bg-primary-800/60 grow rounded-lg border px-3.5 py-2 text-sm transition-all duration-300 outline-none focus:inset-shadow-[4px_4px_10px_1px] focus:inset-shadow-[#A7BBE9]/29 lg:py-2.5 lg:text-base ${classNames}`}
+        type="text"
+        placeholder={placeholder}
+        id={id}
+        value={value}
+        onChange={(e) => onChange(e)}
+      />
+    </div>
+  );
+}
+
+InputField.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  classNames: PropTypes.string,
+};
