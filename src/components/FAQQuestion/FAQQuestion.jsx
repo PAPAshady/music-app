6640b15e4f060dbc95ed52ab65d5a0c06286d642
Nima Zamani <zamani.nima18@gmail.com>
2025-03-03 11:58:22 -0800

    fear(FAQQuestions): added additional styles.

--- src/components/FAQQuestion/FAQQuestion.jsx
+++ src/components/FAQQuestion/FAQQuestion.jsx
@@ -5,10 +5,10 @@ export default function FAQQuestion({ icon, question, answer, classNames }) {
   const styledIcon = cloneElement(icon, { size: '100%' });
   return (
     <div
-      className={`flex flex-col items-center gap-2 text-center lg:items-start lg:gap-3 lg:text-start ${classNames}`}
+      className={`hover:bg-secondary-400/53 flex flex-col items-center gap-2 rounded-2xl p-4 text-center transition-colors duration-300 lg:items-start lg:gap-3 lg:text-start ${classNames}`}
     >
-      <div className="text-primary-500 bg-primary-50 grid size-12 place-content-center rounded-full lg:size-16">
-        <span className="size-6 lg:size-8">{styledIcon}</span>
+      <div className="text-primary-500 bg-primary-50 grid size-12 place-content-center rounded-full lg:size-14">
+        <span className="size-6 lg:size-7">{styledIcon}</span>
       </div>
       <span className="font-semibold lg:text-xl">{question}</span>
       <p className="text-primary-100 text-sm lg:text-base">{answer}</p>
