60538f5ba65a7bb1f7f8dd1b8b42f40e5d66f22c
Nima Zamani <zamani.nima18@gmail.com>
2025-03-08 12:39:33 -0800

    feat: installed react query and supabase and initilized them for the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HamburgerMenuProvider } from './contexts/HamburgerMenuContext';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import routes from './Router';
 import './App.css';
 
+const router = createBrowserRouter(routes);
+const queryClinet = new QueryClient();
+
 function App() {
-  const router = createBrowserRouter(routes);
   return (
-    <HamburgerMenuProvider>
-      <RouterProvider router={router} />
-    </HamburgerMenuProvider>
+    <QueryClientProvider client={queryClinet}>
+      <HamburgerMenuProvider>
+        <RouterProvider router={router} />
+      </HamburgerMenuProvider>
+    </QueryClientProvider>
   );
 }
 
