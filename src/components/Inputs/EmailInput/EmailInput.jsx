7aada9194ad79e00f06df99c278c627085381f1d
Nima Zamani <zamani.nima18@gmail.com>
2025-03-22 13:26:27 +0330

    feat(EmailInput): added 'disabled' prop to and spicific styles.

--- src/components/Inputs/EmailInput/EmailInput.jsx
+++ src/components/Inputs/EmailInput/EmailInput.jsx
@@ -2,15 +2,19 @@ import { useId, forwardRef } from 'react';
 import { Sms } from 'iconsax-react';
 import PropTypes from 'prop-types';
 
-const EmailInput = forwardRef(({ classNames, isInvalid, errorMsg, ...props }, ref) => {
+const EmailInput = forwardRef(({ classNames, isInvalid, errorMsg, disabled, ...props }, ref) => {
   const id = useId();
   return (
     <div className="flex flex-col gap-2">
-      <label className={errorMsg ? 'text-red' : 'text-primary-100'} htmlFor={id}>
-        {errorMsg || 'Email'}
+      <label
+        className={`flex items-center gap-2 ${errorMsg ? 'text-red' : 'text-primary-100'}`}
+        htmlFor={id}
+      >
+        {errorMsg || `Email`}
+        {disabled && <span className="text-sm">(This field cannot be changed.)</span>}
       </label>
       <div
-        className={`bg-primary-900 focus-within:bg-primary-800/60 flex items-center gap-2 rounded-lg border px-3.5 transition-all duration-300 focus-within:inset-shadow-[4px_4px_10px_1px] focus-within:inset-shadow-[#A7BBE9]/29 ${classNames} ${isInvalid ? 'border-red' : 'border-primary-50'}`}
+        className={`focus-within:bg-primary-800/60 flex items-center gap-2 rounded-lg border px-3.5 transition-all duration-300 focus-within:inset-shadow-[4px_4px_10px_1px] focus-within:inset-shadow-[#A7BBE9]/29 ${isInvalid ? 'border-red' : 'border-primary-50'} ${disabled ? 'bg-white-700' : 'bg-primary-900'} ${classNames}`}
       >
         <span className="size-6 lg:size-7">
           <Sms size="100%" />
@@ -22,6 +26,7 @@ const EmailInput = forwardRef(({ classNames, isInvalid, errorMsg, ...props }, re
           placeholder="Email"
           ref={ref}
           id={id}
+          disabled={disabled}
         />
       </div>
     </div>
@@ -35,6 +40,7 @@ EmailInput.propTypes = {
   classNames: PropTypes.string,
   isInvalid: PropTypes.bool,
   errorMsg: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 export default EmailInput;
