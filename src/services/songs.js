50fbf79efea9557a47a2b411c18ea8421e597694
Nima Zamani <zamani.nima18@gmail.com>
2025-03-27 02:55:48 +0330

    refactor(songs.js): refactored functions to fetch more specific data.

--- src/services/songs.js
+++ src/services/songs.js
@@ -14,9 +14,33 @@ export const getAllSongs = async () => {
   }
 };
 
-export const getSongsBy = async (columnName, columnValue) => {
+export const getSongsByAlbum = async (albumName, artist) => {
   try {
-    const { error, data } = await supabase.from('songs').select('*').eq(columnName, columnValue);
+    const { error, data } = await supabase
+      .from('songs')
+      .select('*')
+      .eq('artist', artist)
+      .eq('album', albumName);
+    if (error) throw error;
+    return data;
+  } catch (err) {
+    if (err.code === 'PGRST116') {
+      return null;
+    } else {
+      throw err;
+    }
+  }
+};
+
+// Fetche songs that contain at least one of the specified genres in their genre array.
+export const getSongsByGenres = async (genresArray) => {
+  if (!genresArray.length) return null;
+
+  try {
+    const { error, data } = await supabase
+      .from('songs')
+      .select('*')
+      .or(genresArray.map((genre) => `genres.cs.{${genre.toLowerCase()}}`).join(','));
     if (error) throw error;
     return data;
   } catch (err) {
