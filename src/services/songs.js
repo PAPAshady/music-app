e0b9d1438a2ea3fdc93d2ce7589bed8ead2e62c5
Nima Zamani <zamani.nima18@gmail.com>
2025-03-27 14:35:38 +0330

    refactor(songs): refactored songs.js fucntions for consistent error handling and more specific data fetching.

--- src/services/songs.js
+++ src/services/songs.js
@@ -1,69 +1,78 @@
 import supabase from './supabaseClient';
 
+const handleSongsErrors = (err) => {
+  if (err.code === 'PGRST116') {
+    return { status: 'not_found', success: true, songs: [] };
+  } else {
+    throw err; // other errors will be handled with react query or another try-catch block.
+  }
+};
+
 export const getAllSongs = async () => {
   try {
-    const { data, error } = await supabase.from('songs').select('*');
+    const { data: songs, error } = await supabase.from('songs').select('*');
     if (error) throw error;
-    return data;
+    return { status: 'success', success: true, songs };
   } catch (err) {
-    if (err.code === 'PGRST116') {
-      return null; // if no data found, return null instead of throwing an error
-    } else {
-      throw err; // other errors will be handled with react query or another try-catch block.
-    }
+    return handleSongsErrors(err);
   }
 };
 
 export const getSongsByAlbum = async (albumName, artist) => {
   try {
-    const { error, data } = await supabase
+    const { error, data: songs } = await supabase
       .from('songs')
       .select('*')
       .eq('artist', artist)
       .eq('album', albumName);
     if (error) throw error;
-    return data;
+    return { status: 'success', success: true, songs };
   } catch (err) {
-    if (err.code === 'PGRST116') {
-      return null;
-    } else {
-      throw err;
-    }
+    return handleSongsErrors(err);
   }
 };
 
-// Fetche songs that contain at least one of the specified genres in their genre array.
+// Fetch songs that contain at least one of the specified genres in their genre array.
 export const getSongsByGenres = async (genresArray) => {
-  if (!genresArray.length) return null;
+  if (!genresArray || !genresArray.length) {
+    return { status: 'no_genres_provided', success: false, songs: null };
+  }
 
   try {
-    const { error, data } = await supabase
+    const { error, data: songs } = await supabase
       .from('songs')
       .select('*')
       .or(genresArray.map((genre) => `genres.cs.{${genre.toLowerCase()}}`).join(','));
     if (error) throw error;
-    return data;
+    return { status: 'success', success: true, songs };
   } catch (err) {
-    if (err.code === 'PGRST116') {
-      return null;
-    } else {
-      throw err;
-    }
+    return handleSongsErrors(err);
   }
 };
 
-export const getSingleSong = async (columnName, columnValue) => {
+export const getSongsBy = async (queryObject) => {
   try {
-    const { error, data } = await supabase
-      .from('songs')
-      .select('*')
-      .eq(columnName, columnValue)
-      .single();
+    const { error, data: songs } = await supabase.from('songs').select('*').match(queryObject);
     if (error) throw error;
-    return data;
+    return { status: 'success', success: true, songs };
   } catch (err) {
+    return handleSongsErrors(err);
+  }
+};
+
+export const getSingleSong = async (queryObject) => {
+  try {
+    const { error, data } = await supabase.from('songs').select('*').match(queryObject).single();
+    if (error) throw error;
+    return { status: 'success', success: true, song: data };
+  } catch (err) {
+    // expected 1 result, but found none/multiple result(s)
     if (err.code === 'PGRST116') {
-      return null;
+      if (err.details === 'The result contains 0 rows') {
+        return { status: 'not_found', success: true, song: null };
+      } else {
+        return { status: 'multiple_results', success: false, song: null };
+      }
     } else {
       throw err;
     }
