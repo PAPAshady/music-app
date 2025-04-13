09cab3454f4be1bccf92b6f9ceaa2ce39cc6cd1a
Nima Zamani <zamani.nima18@gmail.com>
2025-04-13 21:42:54 +0330

    fix: modified skeleton loadings aimations and components.
    - fixed the bug for shimmer animation in 'App.css' where shimmer line was not moving across the element perfectly.
    - made a seperate component fot shimmet overlay to use in all skeleton loadings.
    - used ShimmerOverlay.jsx component in 'ArtistCardSkeleton.jsx' and 'MusicPlayerCardSkeleton.jsx'
    - removed unnecessary styles from 'MusicPlayerCardSkeleton.jsx' and 'ArtistCardSkeleton.jsx' components.

--- src/components/MusicCards/ArtistCard/ArtistCardSkeleton.jsx
+++ src/components/MusicCards/ArtistCard/ArtistCardSkeleton.jsx
@@ -1,11 +1,14 @@
+import ShimmerOverlay from '../../ShimmerOverlay/ShimmerOverlay';
+
 export default function ArtistCardSkeleton() {
   return (
-    <div className="inline-flex animate-pulse flex-col items-center gap-5 rounded-lg bg-gray-600/60 py-4 inset-shadow-[2px_2px_6px_-1px] shadow-[4px_4px_8px_1px] shadow-[rgba(0,0,0,0.2)] inset-shadow-transparent transition-all duration-300 lg:py-5 lg:inset-shadow-[4px_4px_8px_1px] lg:hover:inset-shadow-[4px_4px_10px_1px] lg:hover:shadow-[rgba(0,0,0,0.4)] lg:hover:inset-shadow-[#A7BBE9]/29">
+    <div className="relative inline-flex flex-col items-center gap-5 overflow-hidden rounded-lg bg-gray-600/60 py-4 inset-shadow-[2px_2px_6px_-1px] shadow-[4px_4px_8px_1px] shadow-[rgba(0,0,0,0.2)] inset-shadow-transparent transition-all duration-300 lg:py-5 lg:inset-shadow-[4px_4px_8px_1px] lg:hover:inset-shadow-[4px_4px_10px_1px] lg:hover:shadow-[rgba(0,0,0,0.4)] lg:hover:inset-shadow-[#A7BBE9]/29">
+      <ShimmerOverlay />
       <div className="px-6">
-        <div className="size-[72px] overflow-hidden rounded-full bg-gray-800/60 lg:size-[120px]"></div>
+        <div className="size-[72px] rounded-full bg-gray-800/60 lg:size-[120px]"></div>
       </div>
 
-      <p className="text-white-50 flex w-[75%] max-w-[100px] items-center rounded-full bg-gray-800/60 px-3 py-1.5 text-center text-sm lg:max-w-[180px] lg:text-base"></p>
+      <p className="w-[75%] max-w-[100px] rounded-full bg-gray-800/60 px-3 py-1.5 lg:max-w-[180px]"></p>
     </div>
   );
 }
---
+++ src/components/ShimmerOverlay/ShimmerOverlay.jsx
@@ -0,0 +1,5 @@
+export default function ShimmerOverlay() {
+  return (
+    <div className="animate-shimmer absolute inset-0 w-full bg-gradient-to-r from-transparent via-white/20 to-transparent"></div>
+  );
+}
