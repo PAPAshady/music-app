6311787a4ccb21a33c0628d75ebb468a99351cd8
Nima Zamani <zamani.nima18@gmail.com>
2025-04-13 15:27:29 +0330

    feat: implemented play state control
    Added support for multiple play modes:
    - Repeat current song
    - Repeat entire playlist
    - Shuffle playback

--- src/contexts/MusicPlayerContext.jsx
+++ src/contexts/MusicPlayerContext.jsx
@@ -1,36 +1,72 @@
-import { useState, createContext, useEffect, useRef } from 'react';
+import { useState, createContext, useEffect, useRef, useCallback } from 'react';
 import { BASE_URL } from '../services/api';
 import PropTypes from 'prop-types';
 
 const MusicPlayerContext = createContext();
 const music = new Audio();
+const playStateOptions = ['repeat_all', 'repeat_one', 'shuffle'];
 
 export function MusicPlayerProvider({ children }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [playlist, setPlaylist] = useState([]);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [durations, setDurations] = useState({ rawDuration: 0, formatedDuration: '0:00' });
+  const [playState, setPlayState] = useState(playStateOptions[0]);
   const prevSongIndex = useRef(0);
 
-  // get song's full duration
+  const next = useCallback(() => {
+    if (currentSongIndex === playlist.length - 1) {
+      setCurrentSongIndex(0);
+    } else {
+      setCurrentSongIndex((prev) => ++prev);
+    }
+  }, [currentSongIndex, playlist]);
+
+  const prev = () => {
+    if (currentSongIndex === 0) {
+      setCurrentSongIndex(playlist.length - 1);
+    } else {
+      setCurrentSongIndex((prev) => --prev);
+    }
+  };
+
   useEffect(() => {
-    function formatSongDuration() {
+    const formatSongDuration = () => {
       const seconds = Math.floor(music.duration % 60);
       const mins = Math.floor(music.duration / 60);
       const formatedDuration = `${mins}:${seconds < 10 ? `0${seconds}` : seconds}`;
       setDurations({ rawDuration: music.duration, formatedDuration });
-    }
+    };
+
+    const playStateHandler = () => {
+      if (playState === 'repeat_one') {
+        play();
+      } else if (playState === 'shuffle') {
+        // get a random index other than the current song
+        let randomIndex = null;
+        do {
+          randomIndex = Math.floor(Math.random() * playlist.length);
+        } while (randomIndex === currentSongIndex);
+        setCurrentSongIndex(randomIndex);
+      } else {
+        next();
+      }
+    };
 
     music.addEventListener('loadedmetadata', formatSongDuration);
+    music.addEventListener('ended', playStateHandler);
 
-    return () => music.removeEventListener('loadedmetadata', formatSongDuration);
-  }, []);
+    return () => {
+      music.removeEventListener('loadedmetadata', formatSongDuration);
+      music.removeEventListener('ended', playStateHandler);
+    };
+  }, [playState, playlist, next, currentSongIndex]);
 
   // update music src everytime currentSongIndex changes
   useEffect(() => {
     music.src = `${BASE_URL}${playlist[currentSongIndex]?.musicfile}`;
 
-    // make sure to play the song only if user went for the next/prev song and not on the first render.
+    // make sure to play the song ONLY if user went for the next/prev song and NOT on the first render.
     if (prevSongIndex.current !== currentSongIndex) {
       prevSongIndex.current = currentSongIndex;
       play();
@@ -47,28 +83,21 @@ export function MusicPlayerProvider({ children }) {
     setIsPlaying(false);
   }
 
-  const next = () => {
-    if (currentSongIndex === playlist.length - 1) {
-      setCurrentSongIndex(0);
-    } else {
-      setCurrentSongIndex((prev) => ++prev);
-    }
-  };
-
-  const prev = () => {
-    if (currentSongIndex === 0) {
-      setCurrentSongIndex(playlist.length - 1);
-    } else {
-      setCurrentSongIndex((prev) => --prev);
-    }
-  };
-
   // calculate current time of song and return it in a readable format
   const getCurrentTime = () => {
     const seconds = Math.round(music.currentTime % 60);
     const mins = Math.round(music.currentTime / 60);
     return `${mins}:${seconds < 10 ? `0${seconds}` : seconds}`;
   };
+
+  const togglePlayStates = useCallback(() => {
+    const currentPlayStateIndex = playStateOptions.indexOf(playState);
+    if (currentPlayStateIndex === playStateOptions.length - 1) {
+      setPlayState(playStateOptions[0]);
+    } else {
+      setPlayState(playStateOptions[currentPlayStateIndex + 1]);
+    }
+  }, [playState]);
 
   return (
     <MusicPlayerContext.Provider
@@ -85,6 +114,8 @@ export function MusicPlayerProvider({ children }) {
         getCurrentTime,
         playlist,
         setCurrentSongIndex,
+        playState,
+        togglePlayStates,
       }}
     >
       {children}
