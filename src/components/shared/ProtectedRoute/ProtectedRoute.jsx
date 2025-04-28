4931e0d71a92c2daf0d5655666f54ae0c770501b
Nima Zamani <zamani.nima18@gmail.com>
2025-04-28 22:07:17 +0330

    feat: Implemented a loading for ProtectedRoute when user data is fetching.

--- src/components/shared/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/shared/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import useAuth from '../../../hooks/useAuth';
 import { Navigate } from 'react-router-dom';
+import useMediaQuery from '../../../hooks/useMediaQuery';
+import Logo from '../../Logo/Logo';
 
 export default function ProtectedRoute({ children }) {
   const { isLoggedIn, isLoading } = useAuth();
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="relative">
+        <div
+          className={`fixed inset-0 z-20 grid h-[100dvh] w-full place-content-center backdrop-blur-md transition-all duration-300`}
+        >
+          <Logo size={isDesktop ? 'xl' : 'lg'} isLoading />
+        </div>
+        {children}
+      </div>
+    );
   }
 
   if (!isLoggedIn) {
