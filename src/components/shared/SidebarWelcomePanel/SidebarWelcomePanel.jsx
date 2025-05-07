d4f231f308f31ca8ac6b8b9b46713eb191f9c160
Nima Zamani <zamani.nima18@gmail.com>
2025-05-07 21:28:37 +0330

    feat(ui): improve layout for enhanced visual appeal and better UX.

--- src/components/shared/SidebarWelcomePanel/SidebarWelcomePanel.jsx
+++ src/components/shared/SidebarWelcomePanel/SidebarWelcomePanel.jsx
@@ -1,28 +1,28 @@
-import { Music } from 'iconsax-react';
-import SearchInput from '../../Inputs/SearchInput/SearchInput';
-import useInput from '../../../hooks/useInput';
 import MainButton from '../../Buttons/MainButton/MainButton';
+import Logo from '../../Logo/Logo';
 
 export default function SidebarWelcomePanel() {
-  const searchInput = useInput();
-
   return (
     <div className="sticky top-10 hidden xl:block">
-      <div className="bg-secondary-400/40 border-secondary-200 container flex h-[calc(100dvh-100px) max-h-[700px] min-h-[430px] w-[270px] flex-col items-center justify-center gap-6 rounded-xl border px-3 py-6 text-center xl:w-[310px]">
+      <div className="bg-secondary-400/40 border-secondary-200 container flex h-[calc(100dvh-130px)] max-h-[520px] w-[270px] flex-col items-center justify-center gap-6 overflow-y-auto rounded-xl border px-3 py-6 text-center xl:w-[310px]">
         <p className="text-whit text-[1.7rem] font-semibold text-white">No playlist selected</p>
-        <div className="grid place-content-center rounded-md border border-dashed p-6">
-          <i className="size-18">
-            <Music size="100%" />
-          </i>
-        </div>
-        <p className="text-whit text-[1.7rem] font-semibold text-white">Start Listening</p>
+        <Logo size="lg" />
         <p>Browse featured playlists, search for music, or create your own playlist.</p>
-        <SearchInput {...searchInput} />
-        <MainButton title="Create playlist" size="xl"  classNames="!w-full" />
-        <div className="flex items-center justify-between gap-2 w-full">
-          <MainButton title="Featured" size="sm" type='outline' variant='neutral' classNames='!px-3'  /> 
-          <MainButton title="Featured" size="sm" type='outline' variant='neutral' classNames='!px-3'  /> 
-          <MainButton title="Featured" size="sm" type='outline' variant='neutral' classNames='!px-3'  /> 
+        <div className="flex w-full flex-col items-center justify-between gap-3">
+          <MainButton
+            title="Create playlist"
+            size="md"
+            type="outline"
+            variant="neutral"
+            classNames="w-full"
+          />
+          <MainButton
+            title="Search"
+            size="md"
+            type="outline"
+            variant="neutral"
+            classNames="w-full"
+          />
         </div>
       </div>
     </div>
