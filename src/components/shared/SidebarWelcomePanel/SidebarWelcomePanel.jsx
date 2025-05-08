633c01becdca49ffedb855d0cd09ee4b7e4dba32
Nima Zamani <zamani.nima18@gmail.com>
2025-05-08 23:14:52 +0330

    feat: added a min height for SidebarWelcomePanel to avoid shrinking it too much.

--- src/components/shared/SidebarWelcomePanel/SidebarWelcomePanel.jsx
+++ src/components/shared/SidebarWelcomePanel/SidebarWelcomePanel.jsx
@@ -5,11 +5,11 @@ import { useNavigate } from 'react-router-dom';
 
 export default function SidebarWelcomePanel() {
   const { openPlaylistModal } = usePlaylistInfosModal();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="sticky top-10 hidden xl:block">
-      <div className="bg-secondary-400/40 border-secondary-200 container flex h-[calc(100dvh-130px)] max-h-[520px] w-[270px] flex-col items-center justify-center gap-6 overflow-y-auto rounded-xl border px-3 py-6 text-center xl:w-[310px]">
+      <div className="bg-secondary-400/40 border-secondary-200 container flex h-[calc(100dvh-120px)] max-h-[530px] min-h-[460px] w-[270px] flex-col items-center justify-center gap-6 overflow-y-auto rounded-xl border px-3 py-6 text-center xl:w-[310px]">
         <p className="text-whit text-[1.7rem] font-semibold text-white">No playlist selected</p>
         <Logo size="lg" />
         <p>Browse featured playlists, search for music, or create your own playlist.</p>
