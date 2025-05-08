c2a78a30f91c21c51f1e5ebc7101a1112feecce9
Nima Zamani <zamani.nima18@gmail.com>
2025-05-08 12:34:50 +0330

    refactor: moved the logic of Providers in Providers.jsx so App.jsx have mor clear code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HamburgerMenuProvider } from './contexts/HamburgerMenuContext';
-import { SnackbarProvider } from './contexts/SnackbarContext';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
-import { AuthContextProvider } from './contexts/AuthContext';
-import { MobilePlaylistProvider } from './contexts/MobilePlaylistContext';
-import { PlaylistInfosModalProvider } from './contexts/PlaylistInfosModalContext';
-import routes from './Router';
+import Providers from './contexts/Providers';
 import './App.css';
 
-const router = createBrowserRouter(routes);
-const queryClinet = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClinet}>
-      <SnackbarProvider>
-        <AuthContextProvider>
-          <MusicPlayerProvider>
-            <MobilePlaylistProvider>
-              <HamburgerMenuProvider>
-                <PlaylistInfosModalProvider>
-                  <RouterProvider router={router} />
-                </PlaylistInfosModalProvider>
-              </HamburgerMenuProvider>
-            </MobilePlaylistProvider>
-          </MusicPlayerProvider>
-        </AuthContextProvider>
-      </SnackbarProvider>
-    </QueryClientProvider>
-  );
+  return <Providers />;
 }
 
 export default App;
