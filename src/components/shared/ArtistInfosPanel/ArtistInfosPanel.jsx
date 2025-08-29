8521634e6aa61f9314e7396e4a7320746187c04f
Nima Zamani <zamani.nima18@gmail.com>
2025-08-29 11:33:49 +0330

    fix: truncated the bio and title of ArtistInfosPanel and fixed minor layout issiues.

--- src/components/shared/ArtistInfosPanel/ArtistInfosPanel.jsx
+++ src/components/shared/ArtistInfosPanel/ArtistInfosPanel.jsx
@@ -8,16 +8,27 @@ function ArtistInfosPanel() {
     <div className="sticky top-10 hidden xl:block">
       <div className="bg-secondary-400/40 border-secondary-200 flex h-[calc(100dvh-100px)] max-h-[700px] min-h-[430px] w-[270px] flex-col overflow-y-hidden rounded-xl border px-3 py-3 xl:w-[310px] 2xl:h-[calc(100dvh-200px)]">
         <span className="block text-center text-lg font-semibold">About Artist</span>
-        <div className="mx-auto max-h-[35%] min-h-[35%] w-[75%] overflow-hidden px-4 py-3">
+        <div className="mx-auto min-h-[35%] w-[75%] overflow-hidden px-4 py-3">
           <img
-            src={selectedArtist.image ?? noImage}
+            src={noImage}
             alt={selectedArtist.name}
             className="size-full rounded-2xl object-cover"
           />
         </div>
-        <div className="mb-4 flex flex-col items-center gap-2 text-center">
-          <p className="text-secondary-100 text-lg font-semibold">{selectedArtist.full_name}</p>
-          <p className="text-[13px]">{selectedArtist.bio}</p>
+        <div className="h-full">
+          <div className="mb-1 flex flex-col items-center gap-2 text-center">
+            <div className="w-full overflow-hidden">
+              <p
+                className="text-secondary-100 truncate text-lg font-semibold"
+                title={selectedArtist.full_name}
+              >
+                {selectedArtist.full_name}
+              </p>
+            </div>
+            <p className="line-clamp-4 h-20 text-[13px]" title={selectedArtist.bio}>
+              {selectedArtist.bio}
+            </p>
+          </div>
         </div>
         <div className="flex grow flex-col gap-2 overflow-y-auto pe-2 pt-[2px]">
           {Array(10)
