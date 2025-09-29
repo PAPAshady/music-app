09a13425cfc5dd897ebb9e7456e4cdce33688df1
Nima Zamani <zamani.nima18@gmail.com>
2025-09-29 19:18:45 +0330

    feat: implemented like toggle logic for MusicPlayerCard.jsx as well.

--- src/components/MusicCards/MusicPlayerCard/MusicPlayerCard.jsx
+++ src/components/MusicCards/MusicPlayerCard/MusicPlayerCard.jsx
@@ -3,18 +3,27 @@ import { Heart } from 'iconsax-react';
 import noCoverImg from '../../../assets/images/covers/no-cover.jpg';
 import PropTypes from 'prop-types';
 import { formatTime } from '../../../redux/slices/musicPlayerSlice';
+import { useMutation } from '@tanstack/react-query';
+import { likeSongMutationOptions, unlikeSongMutationOptions } from '../../../queries/likes';
+import { useSelector } from 'react-redux';
 
 export default function MusicPlayerCard({
   title,
   artist = 'Unkown artist',
-  isFavorite,
   duration,
   cover = noCoverImg,
   isPlaying,
+  is_liked,
   onClick,
   classNames,
   musicIndex,
+  id,
 }) {
+  const userId = useSelector((state) => state.auth.user.id);
+  const likeHandlerMutation = useMutation(
+    is_liked ? unlikeSongMutationOptions() : likeSongMutationOptions()
+  );
+
   return (
     <div
       className={`border-secondary-300 hover:border-secondary-50 flex items-center gap-2 overflow-hidden rounded-lg border-2 p-2 transition-all duration-300 ${classNames} ${isPlaying ? 'bg-secondary-600/40 backdrop-blur-xs' : '!border-transparent'}`}
@@ -42,7 +51,10 @@ export default function MusicPlayerCard({
         </div>
         <div className="flex items-center justify-between">
           <span className="text-primary-100 text-sm">{formatTime(duration)}</span>
-          <IconButton icon={<Heart className={isFavorite ? 'fill-red text-red' : ''} />} />
+          <IconButton
+            icon={<Heart className={is_liked ? 'fill-secondary-50 text-secondary-50' : ''} />}
+            onClick={() => likeHandlerMutation.mutate({ songId: id, userId })}
+          />
         </div>
       </div>
     </div>
@@ -59,5 +71,6 @@ MusicPlayerCard.propTypes = {
   onClick: PropTypes.func,
   classNames: PropTypes.string,
   id: PropTypes.string.isRequired,
+  is_liked: PropTypes.bool,
   musicIndex: PropTypes.number.isRequired,
 };
