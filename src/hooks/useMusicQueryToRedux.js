688529d7ebe75bc50fda1fa28e1709aaccf558ea
Nima Zamani <zamani.nima18@gmail.com>
2025-10-16 12:55:06 +0330

    fix: fixed the case where if user tries to play a single song after first page load. the music would play and immidiatly pause.

--- src/hooks/useMusicQueryToRedux.js
+++ src/hooks/useMusicQueryToRedux.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getPlaylistByIdQueryOptions } from '../queries/playlists';
 import { getAlbumByIdQueryOptions } from '../queries/albums';
 import { getSongByIdQueryOptions } from '../queries/musics';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import useQueryState from './useQueryState';
 import { useDispatch } from 'react-redux';
 import {
@@ -12,6 +12,7 @@ import {
 import { setSingleSong } from '../redux/slices/playContextSlice';
 import { setCurrentMusic, music } from '../redux/slices/musicPlayerSlice';
 import { getRelatedSongsBySongDataQueryOptions } from '../queries/musics';
+import { useSelector } from 'react-redux';
 
 const queryOptions = {
   playlist: getPlaylistByIdQueryOptions,
@@ -37,7 +38,7 @@ export default function useMusicQueryToRedux() {
   const { getQuery } = useQueryState();
   const queryType = getQuery('type');
   const id = getQuery('id');
-  const isInitialPageLoad = useRef(true);
+  const currentMusic = useSelector((state) => state.musicPlayer.currentMusic);
 
   // Fetch initial media data (playlist, album, or single track)
   const { data } = useQuery({
@@ -57,14 +58,13 @@ export default function useMusicQueryToRedux() {
       const action = actions[queryType];
       dispatch(action(data));
 
-      // For single tracks, also save related songs and play the song. also dont play the song on initial page load. on initial load it is better to user him self play the song.
+      // For single tracks, also save related songs and play the song. also dont play the song on initial page load (if current music dose not exist). on initial load it is better to user him self play the song.
       // thats why we're manipulating music.src and currentMusic directly instead of dispatching setCurrentSongIndex
-      if (queryType === 'track' && relatedSongs && isInitialPageLoad.current) {
+      if (queryType === 'track' && relatedSongs && !currentMusic) {
         dispatch(setSelectedCollectionTracks(relatedSongs));
         music.src = data.song_url;
         dispatch(setCurrentMusic(data));
-        isInitialPageLoad.current = false;
       }
     }
-  }, [data, dispatch, queryType, relatedSongs]);
+  }, [data, dispatch, queryType, relatedSongs, currentMusic]);
 }
