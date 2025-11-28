89f072133792d6f2563d3a1150f2e0a464d9ff14
Nima Zamani <zamani.nima18@gmail.com>
2025-11-28 20:06:17 +0330

    refactor: GenrePanel is now splited into sub components to be more readable.

---
+++ src/components/GenrePanel/GenrePanelPlaylistCardSkeleton.jsx
@@ -0,0 +1,19 @@
+import ShimmerOverlay from '../ShimmerOverlay/ShimmerOverlay';
+
+function PlaylistCardSkeleton() {
+  return (
+    <div className="flex w-[110px] flex-col rounded-xl">
+      <div className="relative mb-2 h-[100px] w-full overflow-hidden rounded-lg bg-gray-600/60">
+        <ShimmerOverlay />
+      </div>
+      <div className="relative h-2 w-3/4 overflow-hidden rounded-full bg-gray-600/60">
+        <ShimmerOverlay />
+      </div>
+      <div className="relative mt-1.5 h-1.5 w-2/3 overflow-hidden rounded-full bg-gray-600/60">
+        <ShimmerOverlay />
+      </div>
+    </div>
+  );
+}
+
+export default PlaylistCardSkeleton;
