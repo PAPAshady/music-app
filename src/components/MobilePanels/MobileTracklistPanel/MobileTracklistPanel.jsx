1d9515b1068fd442febe7688ee75a488e27ba8e3
Nima Zamani <zamani.nima18@gmail.com>
2025-11-28 19:46:37 +0330

    refactor: refactored MobileTracklistPanel to split it into sub components and make it more readable.

--- src/components/MobilePanels/MobileTracklistPanel/MobileTracklistPanel.jsx
+++ src/components/MobilePanels/MobileTracklistPanel/MobileTracklistPanel.jsx
@@ -1,17 +1,12 @@
 import PlayBarSkeleton from '../../MusicCards/PlayBar/PlayBarSkeleton';
 import PlayBar from '../../MusicCards/PlayBar/PlayBar';
-import ShimmerOverlay from '../../ShimmerOverlay/ShimmerOverlay';
-import PropTypes from 'prop-types';
 import IconButton from '../../Buttons/IconButton/IconButton';
-import LoadingSpinner from '../../LoadingSpinner/LoadingSpinner';
 import MainButton from '../../Buttons/MainButton/MainButton';
-import SearchInput from '../../Inputs/SearchInput/SearchInput';
-import useInput from '../../../hooks/useInput';
 import useMediaQuery from '../../../hooks/useMediaQuery';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { showNewSnackbar } from '../../../redux/slices/snackbarSlice';
 import playlistDefaultCover from '../../../assets/images/covers/no-cover.jpg';
-import { useCallback, useState, memo } from 'react';
+import { useCallback, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { openModal } from '../../../redux/slices/playlistInfosModalSlice';
 import { openModal as openConfirmModal } from '../../../redux/slices/confirmModalSlice';
@@ -20,40 +15,30 @@ import { setCurrentCollection } from '../../../redux/slices/playContextSlice';
 import DropDownList from '../../DropDownList/DropDownList';
 import { togglePlayState } from '../../../redux/slices/musicPlayerSlice';
 import { getFavoriteSongsQueryOptions } from '../../../queries/musics';
-import useDebounce from '../../../hooks/useDebounce';
 import {
   Trash,
-  ArrowLeft,
   Play,
-  AddCircle,
   Edit,
   Additem,
   Pause,
   Shuffle,
   RepeateOne,
   RepeateMusic,
-  Music,
 } from 'iconsax-react';
-import {
-  addSongToPrivatePlaylistMutationOptions,
-  removeSongFromPrivatePlaylistMutationOptions,
-} from '../../../queries/playlists';
+import { removeSongFromPrivatePlaylistMutationOptions } from '../../../queries/playlists';
 import {
   getSongsByAlbumIdQueryOptions,
   getSongsByPlaylistIdQueryOptions,
-  getTrendingSongsQueryOptions,
-  getSongsByKeywordQueryOptions,
 } from '../../../queries/musics';
 import usePlayBar from '../../../hooks/usePlayBar';
 import { getAlbumByIdQueryOptions } from '../../../queries/albums';
 import { getPlaylistByIdQueryOptions } from '../../../queries/playlists';
 import { favoriteSongsInfos } from '../../../redux/slices/playContextSlice';
-import { setCurrentQueuelist, setSelectedSong } from '../../../redux/slices/playContextSlice';
+import AddSongPanel from './AddSongPanel';
 
 function MobileTracklistPanel() {
   const tracklistType = useSelector((state) => state.queryState.type);
   const tracklistId = useSelector((state) => state.queryState.id);
-  const searchInput = useInput();
   const dispatch = useDispatch();
   const isLargeMobile = useMediaQuery('(min-width: 420px)');
   const [pendingSongId, setPendingSongId] = useState(null);
@@ -68,7 +53,6 @@ function MobileTracklistPanel() {
   const playingState = useSelector((state) => state.musicPlayer.playingState);
   const isPlaying = useSelector((state) => state.musicPlayer.isPlaying);
   const playingTracklist = useSelector((state) => state.playContext.currentCollection);
-  const { data: trendingSongs, isTrendingSognsPending } = useQuery(getTrendingSongsQueryOptions());
   const { data: selectedPlaylistSongs, isLoading: isPlaylistSongsLoading } = useQuery(
     tracklistType === 'playlist'
       ? getSongsByPlaylistIdQueryOptions(tracklistId)
@@ -76,25 +60,15 @@ function MobileTracklistPanel() {
         ? getSongsByAlbumIdQueryOptions(tracklistId)
         : getFavoriteSongsQueryOptions()
   );
-  const addSongMutation = useMutation(addSongToPrivatePlaylistMutationOptions(tracklistId));
   const removeSongMutation = useMutation(removeSongFromPrivatePlaylistMutationOptions(tracklistId));
-  const searchedValue = searchInput.value.toLowerCase().trim();
-  const debouncedSearchValue = useDebounce(searchedValue, 500);
+
   // Build a list of suggested songs by excluding any songs that already exist in the selected playlist
-  const playlistSongIds = new Set((selectedPlaylistSongs ?? []).map((song) => song.id));
-  const suggestedSongs = (trendingSongs ?? []).filter((song) => !playlistSongIds.has(song.id));
   const { playTracklist } = usePlayBar();
-  const { data: searchedSongs, isPending: isSearchedSongsPending } = useQuery(
-    getSongsByKeywordQueryOptions(debouncedSearchValue)
-  );
-  const hasData =
-    (searchedValue && searchedSongs?.length > 0) || (!searchedValue && trendingSongs?.length > 0);
-  const dataIsloading = isTrendingSognsPending || isSearchedSongsPending;
   const selectedTracklist = tracklistType === 'favorites' ? favoriteSongsInfos : data;
   const isFavorites = selectedTracklist?.tracklistType === 'favorites';
   const isPrivatePlaylist =
     selectedTracklist?.tracklistType === 'playlist' && !selectedTracklist?.is_public;
-  const showAddPanel =
+  const showAddSongPanel =
     isMobilePanelOpen &&
     !selectedTracklist?.is_public &&
     selectedTracklist?.tracklistType !== 'favorites';
@@ -109,36 +83,6 @@ function MobileTracklistPanel() {
       dispatch(isPlaying ? pause() : play());
     }
   };
-
-  const addSongHandler = useCallback(
-    async (songId) => {
-      const isAlreadyAdded = selectedPlaylistSongs.some((song) => song.id === songId);
-
-      if (isAlreadyAdded) {
-        dispatch(
-          showNewSnackbar({
-            message: 'This song already exists in your playlist.',
-            type: 'warning',
-          })
-        );
-        return;
-      }
-
-      try {
-        setPendingSongId(songId);
-        await addSongMutation.mutateAsync(songId);
-        dispatch(showNewSnackbar({ message: 'Song added succefully. Enjoy!', type: 'success' }));
-      } catch (err) {
-        dispatch(
-          showNewSnackbar({ message: 'Error while adding new song to playlist. Try again.' })
-        );
-        console.error('Error adding new song to playlist : ', err);
-      } finally {
-        setPendingSongId(null);
-      }
-    },
-    [dispatch, selectedPlaylistSongs, addSongMutation]
-  );
 
   const removeSongHandler = useCallback(
     async (songId) => {
@@ -291,165 +235,17 @@ function MobileTracklistPanel() {
         </>
       )}
 
-      {showAddPanel && (
-        <div
-          className={`text-secondary-50 bg-primary-800 fixed inset-0 z-[10] size-full pb-4 text-start transition-all duration-300 ${isAddMenuOpen ? 'tranlate-y-0 opacity-100' : 'translate-y-full opacity-0'}`}
-        >
-          <div className="container flex h-full flex-col gap-4">
-            <div className="flex items-center justify-between pt-4">
-              <button onClick={() => setIsAddMenuOpen(false)}>
-                <ArrowLeft size={28} />
-              </button>
-              <p className="font-semibold sm:text-lg">Add to this playlist</p>
-            </div>
-            <div>
-              <SearchInput {...searchInput} />
-            </div>
-            <div className="bg-primary-700 flex grow flex-col gap-5 overflow-y-scroll rounded-md pb-16 min-[400px]:pb-20 min-[480px]:gap-7 sm:pb-24 md:pb-26">
-              {hasData || dataIsloading ? (
-                <>
-                  <div className="px-4 pt-4 min-[480px]:px-6 min-[480px]:pt-6">
-                    <p className="text-xl font-semibold text-white min-[480px]:text-2xl">
-                      {searchedValue ? `Results for "${searchedValue}"` : 'Suggested'}
-                    </p>
-                    {!searchedValue && (
-                      <p className="mt-1 text-sm min-[480px]:mt-3 min-[480px]:text-lg">
-                        See what&apos;s trending
-                      </p>
-                    )}
-                  </div>
-                  <div className="grid grid-cols-1 gap-4 px-3 pb-4 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 lg:gap-x-4">
-                    {searchedValue
-                      ? isSearchedSongsPending
-                        ? Array(6)
-                            .fill()
-                            .map((_, index) => <SuggestedSongSkeleton key={index} />)
-                        : searchedSongs.map((song) => (
-                            <SuggestedSong
-                              key={song.id}
-                              isPending={song.id === pendingSongId}
-                              onAdd={addSongHandler}
-                              song={song}
-                            />
-                          ))
-                      : isTrendingSognsPending
-                        ? Array(6)
-                            .fill()
-                            .map((_, index) => <SuggestedSongSkeleton key={index} />)
-                        : suggestedSongs
-                            .filter((song) => song.title.toLowerCase().includes(searchedValue))
-                            .map((song) => (
-                              <SuggestedSong
-                                key={song.id}
-                                isPending={song.id === pendingSongId}
-                                onAdd={addSongHandler}
-                                song={song}
-                              />
-                            ))}
-                  </div>
-                </>
-              ) : (
-                <div className="size-full p-4">
-                  <div className="flex size-full flex-col items-center justify-center gap-2 rounded-md border border-dashed p-2 text-center">
-                    <Music size={64} className="text-secondary-300" />
-                    <p className="mt-2 px-4 font-semibold text-white">
-                      No songs found matching that keyword.
-                    </p>
-                    <p className="text-sm">Try something else</p>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
+      {showAddSongPanel && (
+        <AddSongPanel
+          isOpen={isAddMenuOpen}
+          setIsOpen={setIsAddMenuOpen}
+          selectedPlaylistSongs={selectedPlaylistSongs}
+          pendingSongId={pendingSongId}
+          setPendingSongId={setPendingSongId}
+        />
       )}
     </>
   );
 }
-
-const SuggestedSong = memo(({ isPending, onAdd, song }) => {
-  const { id, title, cover, artist } = song;
-  const dispatch = useDispatch();
-  const currentMusicId = useSelector((state) => state.musicPlayer.currentMusic?.id);
-  const isPlaying = useSelector((state) => state.musicPlayer.isPlaying);
-  const isCurrentSongPlaying = id === currentMusicId;
-
-  const playOnClick = () => {
-    if (isCurrentSongPlaying) {
-      dispatch(isPlaying ? pause() : play());
-    } else {
-      dispatch(setCurrentQueuelist([song]));
-      dispatch(setCurrentSongIndex(0));
-      dispatch(setSelectedSong(song));
-    }
-  };
-
-  return (
-    <div className="border-secondary-200 flex items-center justify-between gap-2 rounded-sm md:border">
-      <div className="flex grow items-center gap-2 overflow-hidden">
-        <div
-          className="relative h-15 w-15 min-w-[60px] overflow-hidden rounded-sm sm:h-[70px] sm:w-[70px] sm:min-w-[70px]"
-          onClick={playOnClick}
-        >
-          <img
-            src={cover ? cover : playlistDefaultCover}
-            alt={title}
-            className="size-full object-cover"
-          />
-          <button className="absolute inset-0 flex items-center justify-center bg-black/50">
-            {isCurrentSongPlaying && isPlaying ? (
-              <Pause className="fill-white" />
-            ) : (
-              <Play className="fill-white" />
-            )}
-          </button>
-        </div>
-        <div className="flex grow flex-col gap-1.5 overflow-hidden">
-          <p className="truncate text-[0.9rem] min-[480px]:text-base" title={title}>
-            {title}
-          </p>
-          <p className="text-secondary-200 truncate text-xs min-[480px]:text-sm" title={artist}>
-            {artist}
-          </p>
-        </div>
-      </div>
-      {isPending ? (
-        <LoadingSpinner classNames="me-0.5" />
-      ) : (
-        <IconButton
-          icon={<AddCircle />}
-          classNames="min-w-8.5 min-h-8.5 sm:min-w-10 sm:min-h-10 md:me-1"
-          onClick={() => onAdd(id)}
-        />
-      )}
-    </div>
-  );
-});
-
-const SuggestedSongSkeleton = () => {
-  return (
-    <div className="relative flex items-center justify-between gap-2 overflow-hidden rounded-sm bg-gray-600/60">
-      <ShimmerOverlay />
-      <div className="flex grow items-center gap-2 overflow-hidden">
-        <div className="relative h-15 w-15 min-w-[60px] overflow-hidden rounded-sm sm:h-[70px] sm:w-[70px] sm:min-w-[70px]">
-          <div className="size-full bg-gray-800/50"></div>
-        </div>
-        <div className="flex grow flex-col gap-1.5 overflow-hidden p-1">
-          <p className="h-3 w-3/4 rounded-full bg-gray-800/50 sm:w-1/2"></p>
-          <p className="h-3 w-1/2 rounded-full bg-gray-800/50 sm:w-1/3"></p>
-        </div>
-      </div>
-
-      <div className="me-2 min-h-8.5 min-w-8.5 rounded-md bg-gray-800/60 sm:min-h-10 sm:min-w-10"></div>
-    </div>
-  );
-};
-
-SuggestedSong.displayName = 'SuggestedSong';
-SuggestedSong.propTypes = {
-  song: PropTypes.object.isRequired,
-  isPending: PropTypes.bool,
-  onAdd: PropTypes.func.isRequired,
-};
 
 export default MobileTracklistPanel;
