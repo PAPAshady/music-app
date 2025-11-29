94538d26e2469195fe009fa9d9c69f96b8aca1b4
Nima Zamani <zamani.nima18@gmail.com>
2025-11-29 19:07:49 +0330

    refactor: refactored MobileSearchPanel and split into smaller components to improve maintainability.

---
+++ src/components/MobileSearchPanel/MobileSearchPanelArtistsList.jsx
@@ -0,0 +1,27 @@
+import MobileSearchPanelSectionTitle from './MobileSearchPanelSectionTitle';
+import SmallArtistCard from '../MusicCards/SmallArtistCard/SmallArtistCard';
+import SmallArtistCardSkeleton from '../MusicCards/SmallArtistCard/SmallArtistCardSkeleton';
+import { Profile2User } from 'iconsax-react';
+import PropTypes from 'prop-types';
+
+function MobileSearchPanelArtistsList({ artists, isArtistsPending }) {
+  return (
+    <div>
+      <MobileSearchPanelSectionTitle title="Artists" icon={<Profile2User />} />
+      <div className="grid grid-cols-3 gap-4 px-1 min-[500px]:grid-cols-4 min-[900px]:!grid-cols-6 sm:grid-cols-5">
+        {isArtistsPending
+          ? Array(6)
+              .fill()
+              .map((_, index) => <SmallArtistCardSkeleton key={index} size="md" />)
+          : artists.map((artist) => <SmallArtistCard size="md" key={artist.id} artist={artist} />)}
+      </div>
+    </div>
+  );
+}
+
+MobileSearchPanelArtistsList.propTypes = {
+  artists: PropTypes.array,
+  isArtistsPending: PropTypes.bool.isRequired,
+};
+
+export default MobileSearchPanelArtistsList;
---
+++ src/components/MobileSearchPanel/MobileSearchPanelSongsList.jsx
@@ -0,0 +1,44 @@
+import SongCardSkeleton from '../MusicCards/SongCard/SongCardSkeleton';
+import SongCard from '../MusicCards/SongCard/SongCard';
+import { Musicnote } from 'iconsax-react';
+import usePlayBar from '../../hooks/usePlayBar';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { closeMobileSearchPanel } from '../../redux/slices/mobileSearchPanelSlice';
+import PropTypes from 'prop-types';
+import MobileSearchPanelSectionTitle from './MobileSearchPanelSectionTitle';
+
+function MobileSearchPanelSongsList({ songs, isSongsPending }) {
+  const dispatch = useDispatch();
+  const { playSingleSong } = usePlayBar();
+
+  const onPlayTrack = useCallback(
+    (song) => {
+      playSingleSong(song);
+      dispatch(closeMobileSearchPanel());
+    },
+    [dispatch, playSingleSong]
+  );
+
+  return (
+    <div>
+      <MobileSearchPanelSectionTitle title="Songs" icon={<Musicnote />} />
+      <div className="grid grid-cols-1 gap-3 px-1 min-[992px]:!grid-cols-3 sm:grid-cols-2">
+        {isSongsPending
+          ? Array(4)
+              .fill()
+              .map((_, index) => <SongCardSkeleton key={index} />)
+          : songs.map((song, index) => (
+              <SongCard key={song.id} song={song} index={index} onPlay={onPlayTrack} />
+            ))}
+      </div>
+    </div>
+  );
+}
+
+MobileSearchPanelSongsList.propTypes = {
+  songs: PropTypes.array,
+  isSongsPending: PropTypes.bool.isRequired,
+};
+
+export default MobileSearchPanelSongsList;
