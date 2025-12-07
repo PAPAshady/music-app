0bc26880742b3fca4132b08430bb2269add1cbfb
Nima Zamani <zamani.nima18@gmail.com>
2025-12-07 17:14:49 +0330

    fix: fixed the case where MobilePlayerPanel sometimes would show the loading state while the song was already playing.
    - instead of fetching the song from server, we use the redux values (currentMusic) to display the song in the player panel.

--- src/components/MobilePlayerPanel/MobilePlayerPanel.jsx
+++ src/components/MobilePlayerPanel/MobilePlayerPanel.jsx
@@ -13,8 +13,7 @@ import {
 import PropTypes from 'prop-types';
 import PlayerProgressBar from '../PlayerProgressBar/PlayerProgressBar';
 import { useSelector, useDispatch } from 'react-redux';
-import { useQuery, useMutation } from '@tanstack/react-query';
-import { getSongByIdQueryOptions } from '../../queries/musics';
+import { useMutation } from '@tanstack/react-query';
 import {
   formatTime,
   play,
@@ -24,8 +23,6 @@ import {
   togglePlayState,
 } from '../../redux/slices/musicPlayerSlice';
 import { likeSongMutationOptions, unlikeSongMutationOptions } from '../../queries/likes';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
-import ShimmerOverlay from '../ShimmerOverlay/ShimmerOverlay';
 import 'swiper/css';
 import { closePanel } from '../../redux/slices/playerPanelSlice';
 import MobilePlayerPanelTabButton from './MobilePlayerPanelTabButton';
@@ -36,16 +33,11 @@ import LyricsTab from './MobilePlayerPanelTabs/LyricsTab';
 import QueuelistTab from './MobilePlayerPanelTabs/QueuelistTab';
 
 function MobilePlayerPanel() {
-  const songId = useSelector((state) => state.queryState.id);
-  const type = useSelector((state) => state.queryState.type);
-  const { data: song, isPending } = useQuery({
-    ...getSongByIdQueryOptions(songId),
-    enabled: !!songId && type === 'track',
-  });
+  const currentMusic = useSelector((state) => state.musicPlayer.currentMusic);
   const dispatch = useDispatch();
   const isPlaying = useSelector((state) => state.musicPlayer.isPlaying);
   const likeHandlerMutation = useMutation(
-    song?.is_liked ? unlikeSongMutationOptions() : likeSongMutationOptions()
+    currentMusic?.is_liked ? unlikeSongMutationOptions() : likeSongMutationOptions()
   );
 
   const playingState = useSelector((state) => state.musicPlayer.playingState);
@@ -68,9 +60,9 @@ function MobilePlayerPanel() {
   const playButtons = [
     {
       id: 1,
-      icon: <Like1 variant={song?.is_liked ? 'Bold' : 'Linear'} />,
-      classNames: `size-8 sm:size-10 transition-colors duration-300 ${song?.is_liked ? 'text-primary-50 fill-primary-50' : ''}`,
-      onClick: () => likeHandlerMutation.mutate(song?.id),
+      icon: <Like1 variant={currentMusic?.is_liked ? 'Bold' : 'Linear'} />,
+      classNames: `size-8 sm:size-10 transition-colors duration-300 ${currentMusic?.is_liked ? 'text-primary-50 fill-primary-50' : ''}`,
+      onClick: () => likeHandlerMutation.mutate(currentMusic?.id),
     },
     { id: 2, icon: <Previous />, classNames: 'size-8 sm:size-10', onClick: () => dispatch(prev()) },
     {
@@ -106,39 +98,27 @@ function MobilePlayerPanel() {
       <div className="flex w-full grow items-center justify-center pt-20">
         <div className="relative overflow-hidden">
           <img
-            src={song?.cover || musicCover}
+            src={currentMusic?.cover || musicCover}
             className="aspect-square w-[85vw] rounded-xl object-cover min-[500px]:max-w-[420px] min-[500px]:rounded-3xl sm:w-[70vw] sm:max-w-none md:max-w-[650px]"
-            alt={song?.title}
+            alt={currentMusic?.title}
           />
-          <div
-            className={`absolute inset-0 flex size-full items-center justify-center transition-opacity ${isPending ? 'opacity-100' : 'opacity-0'}`}
-          >
-            {isPending && <LoadingSpinner classNames="z-[1]" size="lg" />}
-            <div className="absolute size-full bg-[#000]/50"></div>
-          </div>
         </div>
       </div>
       <div className="flex w-full flex-col gap-4 px-4 pb-20 text-start sm:w-[95%]">
-        {isPending ? (
-          <div className="flex flex-col gap-3 sm:mb-2">
-            <div className="relative h-3.5 w-2/3 overflow-hidden rounded-full bg-gray-600/60">
-              <ShimmerOverlay />
-            </div>
-            <div className="relative h-2.5 w-1/3 overflow-hidden rounded-full bg-gray-600/60">
-              <ShimmerOverlay />
-            </div>
-          </div>
-        ) : (
-          <div className="sm:mb-2">
-            <p className="text-secondary-50 text-2xl font-bold sm:text-4xl">{song?.title}</p>
-            <p className="text-secondary-200 mt-1 text-sm sm:mt-4 sm:text-xl">{song?.artist}</p>
-          </div>
-        )}
+        <div className="sm:mb-2">
+          <p className="text-secondary-50 text-2xl font-bold sm:text-4xl">{currentMusic?.title}</p>
+          <p className="text-secondary-200 mt-1 text-sm sm:mt-4 sm:text-xl">
+            {currentMusic?.artist}
+          </p>
+        </div>
+
         <div className="text-secondary-100">
           <PlayerProgressBar />
           <div className="mt-3 flex items-center justify-between text-xs">
             <MobilePlayerPanelCurrentTimeNumber />
-            <span className="text-primary-100 text-sm">{formatTime(song?.duration || 0)}</span>
+            <span className="text-primary-100 text-sm">
+              {formatTime(currentMusic?.duration || 0)}
+            </span>
           </div>
         </div>
         <div className="text-secondary-100 flex items-center justify-between px-2 min-[380px]:px-4">
@@ -152,11 +132,7 @@ function MobilePlayerPanel() {
       >
         <div>
           {isContentPanelOpen && (
-            <MobilePlayerPanelContentPanel
-              song={song}
-              isPending={isPending}
-              onClose={closeContentPanel}
-            />
+            <MobilePlayerPanelContentPanel song={currentMusic} onClose={closeContentPanel} />
           )}
           <div onClick={() => setIsContentPanelOpen(true)}>
             <div className="border-secondary-300 flex items-center border-b md:-mt-3">
@@ -175,9 +151,9 @@ function MobilePlayerPanel() {
         <div
           className={`overflow-y-auto p-4 transition-opacity duration-300 ${isContentPanelOpen ? 'opacity-100' : 'opacity-0'}`}
         >
-          {tab === 'RELATED' && <RelatedTab song={song} onClose={closePlayerPanel} />}
-          {tab === 'LYRICS' && <LyricsTab song={song} />}
-          {tab === 'UP NEXT' && <QueuelistTab song={song} />}
+          {tab === 'RELATED' && <RelatedTab onClose={closePlayerPanel} />}
+          {tab === 'LYRICS' && <LyricsTab />}
+          {tab === 'UP NEXT' && <QueuelistTab />}
         </div>
       </div>
     </div>
