dccfcaab50319228b566985bab6aca705e7727fe
Nima Zamani <zamani.nima18@gmail.com>
2025-12-08 17:48:11 +0330

    feat: implemented logic to subscribe playlists from MobileTracklistPanel.

--- src/components/MobilePanels/MobileTracklistPanel/MobileTracklistPanelPlayButtons.jsx
+++ src/components/MobilePanels/MobileTracklistPanel/MobileTracklistPanelPlayButtons.jsx
@@ -5,16 +5,27 @@ import { openModal } from '../../../redux/slices/playlistInfosModalSlice';
 import { openModal as openConfirmModal } from '../../../redux/slices/confirmModalSlice';
 import { Edit, Trash } from 'iconsax-react';
 import PropTypes from 'prop-types';
+import MainButton from '../../Buttons/MainButton/MainButton';
+import { useMutation } from '@tanstack/react-query';
+import {
+  unsubscribeFromPlaylistMutationOptions,
+  subscribeToPlaylistMutationOptions,
+} from '../../../queries/playlists';
 
-function MobileTracklistPanelPlayButtons({ tracklistTitle, setIsAddMenuOpen }) {
+function MobileTracklistPanelPlayButtons({ playlist, setIsAddMenuOpen }) {
   const dispatch = useDispatch();
+  const playlistSubscriptionMutation = useMutation(
+    playlist?.is_subscribed
+      ? unsubscribeFromPlaylistMutationOptions()
+      : subscribeToPlaylistMutationOptions()
+  );
 
-  const playButtons = [
+  const privatePlaylistButtons = [
     {
       id: 1,
       icon: <Edit />,
       onClick: () =>
-        dispatch(openModal({ title: `Edit ${tracklistTitle}`, actionType: 'edit_playlist' })),
+        dispatch(openModal({ title: `Edit ${playlist?.title}`, actionType: 'edit_playlist' })),
     },
     { id: 2, icon: <Additem />, onClick: () => setIsAddMenuOpen(true) },
     {
@@ -24,7 +35,7 @@ function MobileTracklistPanelPlayButtons({ tracklistTitle, setIsAddMenuOpen }) {
       onClick: () =>
         dispatch(
           openConfirmModal({
-            title: `Delete "${tracklistTitle}" playlist.`,
+            title: `Delete "${playlist?.title}" playlist.`,
             message: 'Are you sure you want to delete this playlist ?',
             buttons: { confirm: true, cancel: true },
             buttonsClassNames: { confirm: '!bg-red !inset-shadow-none' },
@@ -34,14 +45,24 @@ function MobileTracklistPanelPlayButtons({ tracklistTitle, setIsAddMenuOpen }) {
     },
   ];
 
-  return playButtons.map((button) => (
-    <IconButton key={button.id} classNames="sm:size-9 md:size-10" {...button} />
-  ));
+  return playlist?.is_public ? (
+    <MainButton
+      onClick={() => playlistSubscriptionMutation.mutate(playlist.id)}
+      size="sm"
+      title={playlist.is_subscribed ? 'Unsubscribe' : 'Subscribe'}
+      variant="secondary"
+      disabled={playlistSubscriptionMutation.isPending}
+    />
+  ) : (
+    privatePlaylistButtons.map((button) => (
+      <IconButton key={button.id} classNames="sm:size-9 md:size-10" {...button} />
+    ))
+  );
 }
 
 export default MobileTracklistPanelPlayButtons;
 
 MobileTracklistPanelPlayButtons.propTypes = {
-  tracklistTitle: PropTypes.string,
+  playlist: PropTypes.object,
   setIsAddMenuOpen: PropTypes.func.isRequired,
 };
